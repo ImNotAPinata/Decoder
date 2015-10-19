6c072c41db1e3ece7aa920c1316309d40395c944
ImNotAPinata <jerrysb7788@gmail.com>
2015-10-18 19:43:53 -0500

    JS BarDecoder Sample

---
+++ BarDecoder/Decodificador.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace BarDecoder
+{
+    partial class Decodificador
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGetPath = new System.Windows.Forms.Button();
+            this.txtPath = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblcode = new System.Windows.Forms.Label();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnGetPath
+            // 
+            this.btnGetPath.Location = new System.Drawing.Point(507, 33);
+            this.btnGetPath.Name = "btnGetPath";
+            this.btnGetPath.Size = new System.Drawing.Size(51, 22);
+            this.btnGetPath.TabIndex = 0;
+            this.btnGetPath.Text = "...";
+            this.btnGetPath.UseVisualStyleBackColor = true;
+            this.btnGetPath.Click += new System.EventHandler(this.btnGetPath_Click);
+            // 
+            // txtPath
+            // 
+            this.txtPath.Location = new System.Drawing.Point(115, 33);
+            this.txtPath.Name = "txtPath";
+            this.txtPath.Size = new System.Drawing.Size(386, 22);
+            this.txtPath.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Image Path:";
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.pictureBox.Location = new System.Drawing.Point(29, 70);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(529, 298);
+            this.pictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox.TabIndex = 3;
+            this.pictureBox.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(26, 398);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Codigo:";
+            // 
+            // lblcode
+            // 
+            this.lblcode.AutoSize = true;
+            this.lblcode.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblcode.Location = new System.Drawing.Point(112, 398);
+            this.lblcode.Name = "lblcode";
+            this.lblcode.Size = new System.Drawing.Size(77, 17);
+            this.lblcode.TabIndex = 5;
+            this.lblcode.Text = "-- code --";
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.FileName = "openFileDialog";
+            this.openFileDialog.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog_FileOk);
+            // 
+            // Decodificador
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(587, 454);
+            this.Controls.Add(this.lblcode);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtPath);
+            this.Controls.Add(this.btnGetPath);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Decodificador";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Decodificador";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGetPath;
+        private System.Windows.Forms.TextBox txtPath;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblcode;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+    }
+}
+
---
+++ BarDecoder/Decodificador.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ZXing;
+
+namespace BarDecoder
+{
+    public partial class Decodificador : Form
+    {
+        public Decodificador()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGetPath_Click(object sender, EventArgs e)
+        {
+            openFileDialog.ShowDialog();
+        }
+
+        private void openFileDialog_FileOk(object sender, CancelEventArgs e)
+        {
+            txtPath.Text = openFileDialog.FileName;
+            bool IsValid = ValidateFile(txtPath.Text);
+            if (IsValid)
+            {
+                var codeBitMap = (Bitmap) Bitmap.FromFile(txtPath.Text); 
+                pictureBox.Image = codeBitMap;
+
+                IBarcodeReader reader = new BarcodeReader();
+                var code = reader.Decode(codeBitMap);
+                if (code != null)
+                {
+                    lblcode.Text = code.Text.ToString();
+                }
+                else { lblcode.Text = "*** FAILED ON DECODING A CODE FROM IMAGE ***"; }
+            }
+            else
+            {
+                lblcode.Text = "*** ERROR LOADING VALID IMAGE ***";
+            }
+        }
+
+        private bool ValidateFile(string p)
+        {
+            var extension = System.IO.Path.GetExtension(p);
+            List<String> ImagesExtensions = new List<string> { ".JPG", ".JPE", ".BMP", ".GIF", ".PNG" };
+
+            var IsValidExtension = ImagesExtensions.Contains(extension.ToUpper());
+            return IsValidExtension;
+        }
+
+
+
+    }
+}
